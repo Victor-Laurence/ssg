5f48c2bd73af54ca406224a1c5b96c1796e6e2c3
Victor <victor@victorlaurence.com>
2024-11-18 21:00:01 -0800

    5.1 - Copy Static

--- src/main.py
+++ src/main.py
@@ -2,28 +2,31 @@ import os
 import shutil
 
 def main():
-    #clean_public()
-    #copy_static_files()
-    pass
-
-
-def clean_public():
     root = os.getcwd()
+    static = os.path.join(root, "static")
     public = os.path.join(root, "public")
 
-    print(public)
-    print(os.path.exists(public))
+    clean_dir(public)
+    copy_dir(static, public)
+
+
+def clean_dir(public):
     if os.path.exists(public):
         shutil.rmtree(public)
-    print(os.path.exists(public))
     if not os.path.exists(public):
-        os.mkdir(public)
-    print(os.path.exists(public))
-    
+        os.mkdir(public)    
 
-def copy_static_files():
-    pass
 
+def copy_dir(source, destination):
+    for path in os.listdir(source):
+        full_source_path = os.path.join(source, path)
+        full_dest_path = os.path.join(destination, path)
+
+        if os.path.isdir(full_source_path):
+            os.mkdir(full_dest_path)
+            copy_dir(full_source_path, full_dest_path)
+        elif os.path.isfile(full_source_path):
+            shutil.copy(full_source_path, full_dest_path)
 
 
 if __name__ == "__main__":
